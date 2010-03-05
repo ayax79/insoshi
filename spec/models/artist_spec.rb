42153d6302a3a6d2c3c73a9aa98931a36af0b7fe
Andrew Wright <ayax79@gmail.com>
2010-03-05 11:08:10 -0800

    more testing for artist.

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Artist do
 
   before(:each) do
-    @artist = artists(:one)
+    @artist = artists(:foobars)
   end
 
   it "should exist" do
@@ -15,15 +15,60 @@ describe Artist do
     p.errors.on(:name).should_not be_nil
   end
 
+  it "should have a name equal to foobars" do
+    @artist.name.should eql('foobars')
+  end
+
+  it "should have the correct bio" do
+    @artist.bio.should_not be_nil
+    @artist.bio.should eql('We are a band and we suck.')
+  end
+
   it "should have members" do
     @artist.members.should be_a_kind_of(Array)
+    @artist.members.should have(2).items
+    @artist.members.should contain_person_with_name('Quentin')
+    @artist.members.should contain_person_with_name('Aaron')
+    @artist.members.should_not contain_person_with_name('admin')
+    @artist.members.should_not contain_person_with_name('Kelly')
   end
 
   it "should have fans" do
     @artist.fans.should be_a_kind_of(Array)
+    @artist.fans.should have(2).items
+    @artist.fans.should_not contain_person_with_name('Quentin')
+    @artist.fans.should_not contain_person_with_name('Aaron')
+    @artist.fans.should contain_person_with_name('admin')
+    @artist.fans.should contain_person_with_name('Kelly')
   end
 
   private
+
+  def contain_person_with_name(name)
+    return ContainPersonWithName.new(name)
+  end
+
+  class ContainPersonWithName
+    def initialize(expected)
+      @expected = expected
+    end
+    def matches?(target)
+      @target = target
+
+      found = false
+      target.each do |person|
+        found = true if person.name == @expected
+      end
+      found
+    end
+    
+    def failure_message
+      "expected #{@target.inspect} to contain person with name #{@expected}"
+    end
+    def negative_failure_message
+      "expected #{@target.inspect} not contain person with name #{@expected}"
+    end
+  end
 
   def create_artist(options ={})
     a = Artist.new(options)
@@ -31,5 +76,5 @@ describe Artist do
     a.save if options[:save]
     a
   end
-  
+
 end
