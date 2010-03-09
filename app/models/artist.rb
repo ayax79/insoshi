46f7a02540d979a78c05d0060dbfab3c0d54c304
Andrew Wright <ayax79@gmail.com>
2010-03-09 14:22:26 -0800

    a bit of artist profile cleanup.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,7 @@
 class Artist < ActiveRecord::Base
 
+  MAX_DEFAULT_CONTACTS = 12
+
   has_and_belongs_to_many :members, :class_name => 'Person', :join_table => 'artists_members'
   has_and_belongs_to_many :fans, :class_name => 'Person', :join_table => 'artists_fans'
   has_many :artist_invites, :dependent => :destroy
@@ -10,7 +12,17 @@ class Artist < ActiveRecord::Base
   #photo helpers
 
   def thumbnail
+    #todo - implement
     "default_thumbnail.png"
+  end
+
+   def other_photos
+    photos.length > 1 ? photos - [photo] : []
+  end
+
+  def main_photo
+    #todo - implement
+    "default.png"
   end
 
 end
