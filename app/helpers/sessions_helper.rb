2308fbb135f2645e26c526a34c59f1228a1ad3b6
A.J. Wright <ayax79@gmail.com>
2010-03-21 14:46:52 -0700

    rpx urls files.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,11 @@ module SessionsHelper
     @rpx.signin_url(dest)
   end
 
+  def rpx_embed_url
+    dest = url_for :controller => :sessions, :action => :rpx_return, :only_path => false
+    "#{rp_url}/openid/embed?token_url=#{CGI.escape(dest)}"
+  end
+
   def rpx_map_url
     dest = url_for :controller => :sessions, :action => :rpx_map_return, :only_path => false
     @rpx.signin_url(dest)
@@ -15,6 +20,14 @@ module SessionsHelper
        href="#{rpx_token_url}">
       Use an external Account
     </a>)
+  end
+
+  private
+
+  def rp_url
+    parts = RPX_BASE_URL.split('://', 2)
+    parts = parts.insert(1, '://' + RPX_REALM + '.')
+    return parts.join('')
   end
 
 end
