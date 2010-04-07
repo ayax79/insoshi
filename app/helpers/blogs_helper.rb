0b0cf6f6ed63a24178d1c3386e607aab9cc7b2c9
A.J. Wright <ayax79@gmail.com>
2010-04-06 17:42:43 -0700

    Added blog_editable? method.

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -5,5 +5,13 @@ module BlogsHelper
 
   def blog_tab_url(blog)
     person_url(blog.person, :anchor => "tBlog")
-  end  
+  end
+
+  def blog_editable?(blog)
+    unless blog.artist.nil?
+      blog.artist.member?(current_person)
+    else
+      current_person?(@blog.person)
+    end
+  end
 end
