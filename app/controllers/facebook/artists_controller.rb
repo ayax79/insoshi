27eaa6a506a16c9cd5a2f48165261039f035a2a2
A.J. Wright <ayax79@gmail.com>
2010-05-05 18:08:02 -0700

    working on tab stuff.

--- app/controllers/facebook/artists_controller.rb
+++ app/controllers/facebook/artists_controller.rb
@@ -2,7 +2,9 @@ class Facebook::ArtistsController < ApplicationController
   layout :facebook
   
   def show
-    @artist = Artist.find(params[:id])
+    #@artist = Artist.find(params[:id])
+
+    @owner_id = params[:fb_sig_profile_user]
 
     respond_to do |format|
       format.fbml # show.fbml.erb
